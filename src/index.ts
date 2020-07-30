1169d720746671fc6aa599b167d8beffff5e6c87
tlrhks2869 <tlrhks2869@naver.com>
2020-07-30 18:08:33 +0900

    blockChain

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,26 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
+  }
 }
+const genesisBlock: Block = new Block(0, "2020202020202", "", "Hello", 123456);
 
-const person = {
-  name: "kwan",
-  age: 27,
-  gender: "male",
-};
+let blockchain: [Block] = [genesisBlock];
 
-const sayHi = (person: Human): string => {
-  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
-};
-console.log(sayHi(person));
-
+console.log(blockchain);
 export {};
